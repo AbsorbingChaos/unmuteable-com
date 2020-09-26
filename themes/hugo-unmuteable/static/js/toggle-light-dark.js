b6ca0094a78fa8c300bac4ef90a35d2b3a2ca9b9
Jason Schrader <whoabuddy@users.noreply.github.com>
2020-09-26 10:46:58 -0700

    save theme setting to localstorage

--- themes/hugo-unmuteable/static/js/toggle-light-dark.js
+++ themes/hugo-unmuteable/static/js/toggle-light-dark.js
@@ -1,5 +1,18 @@
 const btn = document.querySelector(".ux__header--toggle");
+const currentTheme = localstorage.getItem("theme")
+
+if (currentTheme == "dark") {
+  document.body.classList.add("darkness");
+}
 
 btn.addEventListener("click", function() {
+  
   document.documentElement.classList.toggle("darkness");
+  
+  let theme = "light";
+  if (document.body.classList.contains("darkness")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+
 });
